dc9419c8635ff3667a386c54ce7a71ee8acafb32
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-27 16:45:53 +0800

    [parking-lot-story1][Jackie] feat: implemented ParkingBoy fetch when given a ticket test

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyTest {
     @Test
@@ -14,5 +15,17 @@ class ParkingBoyTest {
         Ticket ticket = parkingBoy.park(car);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_car_when_park_given_ticket() {
+        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+
+        Ticket ticket = parkingBoy.park(car);
+        Car actual = parkingBoy.fetch(ticket);
+
+        assertEquals(car,actual);
     }
 }
