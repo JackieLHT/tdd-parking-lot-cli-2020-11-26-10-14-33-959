1289a80910375b23b6d9eafbab0ba5ad0c667aec
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-29 02:14:41 +0800

    [parking-lot-story6][Jackie] feat: added manager will throw same exceptions as parkingboy when assignPark and assignFetch given a specific parkingboy test

--- src/test/java/com/oocl/cultivation/ExceptionTest.java
+++ src/test/java/com/oocl/cultivation/ExceptionTest.java
@@ -115,5 +115,50 @@ public class ExceptionTest {
 
     }
 
+    @Test
+    public void should_throw_NotEnoughPositionException_when_assignPark_given_a_car_and_a_specific_parkingboy_and_all_parkinglots_are_full() throws NotEnoughPositionException {
+        ParkingLot parkinglot1 = new ParkingLot(1);
+        ParkingLot parkinglot2 = new ParkingLot(1);
+        List<ParkingLot> parkingLotList1 = new ArrayList<>();
+        List<ParkingLot> parkingLotList2 = new ArrayList<>();
+        parkingLotList1.add(parkinglot1);
+        parkingLotList2.add(parkinglot2);
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLotList1);
+        Manager manager = new Manager(parkingLotList2);
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        parkinglot1.park(car1);
+
+        assertThrows(NotEnoughPositionException.class, () -> {
+            manager.assignPark(standardParkingBoy,car2)
+        }, "Not enough position.");
+
+    }
+
+    @Test
+    public void should_throw_UnrecognizedParkingTicketException_when_assignFetch_given_a_fake_or_used_tcket_and_a_specific_parkingboy() throws NotEnoughPositionException, UnrecognizedParkingTicketException {
+        ParkingLot parkinglot1 = new ParkingLot(1);
+        ParkingLot parkinglot2 = new ParkingLot(1);
+        List<ParkingLot> parkingLotList1 = new ArrayList<>();
+        List<ParkingLot> parkingLotList2 = new ArrayList<>();
+        parkingLotList1.add(parkinglot1);
+        parkingLotList2.add(parkinglot2);
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLotList1);
+        Manager manager = new Manager(parkingLotList2);
+        Car car1 = new Car();
+        Ticket ticket1 = new Ticket();
+        Ticket ticket2 = parkinglot1.park(car1);
+
+        assertThrows(NotEnoughPositionException.class, () -> {
+            manager.assignFetch(standardParkingBoy,ticket1)
+        }, "Unrecognized parking ticket.");
+
+        assertThrows(NotEnoughPositionException.class, () -> {
+            manager.assignFetch(standardParkingBoy,ticket2);
+        }, "Unrecognized parking ticket.");
+
+    }
+
 
 }
