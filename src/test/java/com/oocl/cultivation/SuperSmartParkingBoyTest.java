448467ee1b4e722ca588f2dd0a19a42fb1a88158
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-28 20:49:48 +0800

    [parking-lot-story5][Jackie] feat: implemented should park car in parkinglot with largest available position rate when SuperSmartParkingBoy given a car parks and multiple parkinglots with empty positions and some parkinglots are full test

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -28,4 +28,26 @@ public class SuperSmartParkingBoyTest {
 
         assertSame(car4, parkingLot2.fetch(ticket));
     }
+
+    @Test
+    public void shoud_park_car_in_parkinglot_with_most_empty_positions_when_park_given_a_car_and_multiple_parking_lots_with_empty_spaces_and_some_parkinglots_are_full() throws  NotEnoughPositionException, UnrecognizedParkingTicketException{
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(3);
+        ParkingLot parkingLot3 = new ParkingLot(2);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        parkingLot1.park(car1);
+        parkingLot2.park(car2);
+
+
+        Ticket ticket = superSmartParkingBoy.park(car3);
+
+        assertSame(car3, parkingLot3.fetch(ticket));
+    }
 }
