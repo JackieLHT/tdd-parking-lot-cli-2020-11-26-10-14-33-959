99464c328471a5857c47c2ab36cc136551baa3d7
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-28 17:53:15 +0800

    [parking-lot-story1][Jackie] feat: implemented throw NotEnoughPositionException when fetch given a car and parkinglot is full test

--- src/test/java/com/oocl/cultivation/ExceptionTest.java
+++ src/test/java/com/oocl/cultivation/ExceptionTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExceptionTest {
     @Test
-    public void should_throw_Unrecognized_parking_ticket_exception_when_park_given_fake_ticket() throws UnrecognizedParkingTicketException {
+    public void should_throw_UnrecognizedParkingTicketException_when_fetch_given_fake_ticket() throws UnrecognizedParkingTicketException {
         ParkingLot parkinglot = new ParkingLot(10);
         ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
         Ticket ticket = new Ticket();
@@ -15,4 +15,21 @@ public class ExceptionTest {
         },"Unrecognized parking ticket");
 
     }
+
+    @Test
+    public void should_throw_NotEnoughPositionException_when_park_given_a_car_and_parkinglot_is_full() throws NotEnoughPositionException {
+        ParkingLot parkinglot = new ParkingLot(1);
+        ParkingBoy parkingBoy = new ParkingBoy(parkinglot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        parkinglot.park(car1);
+
+        assertThrows(UnrecognizedParkingTicketException.class,()->{
+            parkingBoy.park(car2);
+        },"Not enough position.");
+
+    }
+
+
 }
