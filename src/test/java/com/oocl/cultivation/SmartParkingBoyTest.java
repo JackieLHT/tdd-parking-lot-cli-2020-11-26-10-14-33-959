4f5c8ab6b3df474753f3da9733f29e75c1d246ae
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-28 19:26:19 +0800

    [parking-lot-story3][Jackie] feat: implemented should park car in parkinglot with most empty positions when SmartParkingBoy given a car parks and multiple parkinglots with empty positions test

---
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -0,0 +1,27 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class SmartParkingBoyTest {
+    @Test
+    public void shoud_park_car_in_parkinglot_with_most_empty_positions_when_park_given_a_car_and_multiple_parking_lots_with_empty_spaces() {
+        ParkingLot parkingLot1 = new ParkingLot(5);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        ParkingLot parkingLot3 = new ParkingLot(20);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+
+        Ticket ticket = smartParkingBoy.park(car);
+
+        assertSame(car, parkingLot3.fetch(ticket));
+    }
+}
