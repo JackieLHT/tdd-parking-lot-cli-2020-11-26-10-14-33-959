4a87f99cdf625469d0b7072920d0a9108ea41c64
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-28 20:06:28 +0800

    [parking-lot-story4][Jackie] feat: implemented should park car in parkinglot with most empty positions when SmartParkingBoy given a car parks and multiple parkinglots and some parkinglots are full test

--- src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SmartParkingBoyTest.java
@@ -24,4 +24,24 @@ public class SmartParkingBoyTest {
 
         assertSame(car, parkingLot3.fetch(ticket));
     }
+    @Test
+    public void shoud_park_car_in_parkinglot_with_most_empty_positions_when_park_given_a_car_and_multiple_parking_lots_with_empty_spaces_and_some_parkinglots_are_full() throws  NotEnoughPositionException, UnrecognizedParkingTicketException{
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(20);
+        ParkingLot parkingLot3 = new ParkingLot(1);
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        parkingLot1.park(car1);
+        parkingLot3.park(car2);
+
+        Ticket ticket = smartParkingBoy.park(car3);
+
+        assertSame(car3, parkingLot2.fetch(ticket));
+    }
 }
